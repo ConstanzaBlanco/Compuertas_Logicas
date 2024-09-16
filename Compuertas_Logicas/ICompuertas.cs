033d87e935f9709f678fe2129cc1af288d53ad08
ConstanzaBlanco <constanza.blanco@correo.ucu.edu.uy>
2024-09-16 11:34:13 -0300

    entradas

--- Compuertas_Logicas/ICompuertas.cs
+++ Compuertas_Logicas/ICompuertas.cs
@@ -3,6 +3,9 @@
 public interface ICompuertas
 {
     public string name { get; set; }
+    
+    public List<bool> entradas { get; set; }
+    
     public bool AgregarEntrada(string nombre, bool valor);
     public bool Calcular();
     
